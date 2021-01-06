0b1a14ce8b57f542f3182008a484567c365243ad
Jerry Doran <jmdwebtechnologies@gmail.com>
2021-01-06 13:16:11 -0500

    Finished Project Build out the navbar side menu project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import "./styles.css";
+import Home from "./pages/Home";
+import Reports from "./pages/Reports";
+import Products from "./pages/Products";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>App component</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/products" component={Products} />
+        </Switch>
+      </Router>
+    </>
   );
 }
